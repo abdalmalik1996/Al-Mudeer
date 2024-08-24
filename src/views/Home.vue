d16a96731e3f1950fa55d55a7aca6ca373d091df
abdalmalik1996 <abd.almalik96@gmail.com>
2024-08-24 16:34:22 +0300

    delete fix loader test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,29 +13,13 @@
 </template>
 
 <script setup>
-// import Introdacution from "@/components/Introdacution.vue";
-import { defineAsyncComponent } from "vue";
-import LoadingComponent from "@/components/LoadingComponent.vue";
+import Introdacution from "@/components/Introdacution.vue";
 import About from "@/components/About.vue";
 import Teame from "@/components/Teame.vue";
-// import Projects from "@/components/Projects.vue";
+import Projects from "@/components/Projects.vue";
 import Application from "@/components/Application.vue";
 import AboutCompany from "@/components/AboutCompany.vue";
 import Partner from "@/components/Partner.vue";
 import Connect from "@/components/Connect.vue";
 import Certificate from "@/components/Certificate.vue";
-
-
-const Introdacution = defineAsyncComponent({
-  loader: () => import('@/components/Introdacution.vue'),
-  loadingComponent: LoadingComponent /* shows while loading */,
-  delay: 1000 /* delay in ms before showing loading component */,
-  timeout: 3000 /* timeout after this many ms */,
-});
-const Projects = defineAsyncComponent({
-  loader: () => import('@/components/Projects.vue'),
-  loadingComponent: LoadingComponent /* shows while loading */,
-  delay: 1000 /* delay in ms before showing loading component */,
-  timeout: 3000 /* timeout after this many ms */,
-})
 </script>
